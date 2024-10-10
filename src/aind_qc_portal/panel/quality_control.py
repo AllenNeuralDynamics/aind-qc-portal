c5981d7bdd080006d5f8ab60bcdb9e3e4b56e16f
Dan Birman <danbirman@gmail.com>
2024-10-10 15:16:15 -0700

    feat: adding support for modality/stage filters in QC view

--- src/aind_qc_portal/panel/quality_control.py
+++ src/aind_qc_portal/panel/quality_control.py
@@ -3,20 +3,21 @@ import json
 
 import panel as pn
 import param
-from aind_data_schema.core.quality_control import QCEvaluation, QualityControl
+from aind_data_schema.core.quality_control import QualityControl
 
 from aind_qc_portal.docdb.database import qc_from_id, qc_update_to_id
 from aind_qc_portal.panel.evaluation import QCEvalPanel
-from aind_qc_portal.panel.metric import QCMetricPanel
-from aind_qc_portal.utils import md_style, status_html, update_schema_version
+from aind_qc_portal.utils import status_html, update_schema_version
 
 
-class QCPanel:
+class QCPanel(param.Parameterized):
     modality_filter = param.String(default="All")
     stage_filter = param.String(default="All")
 
-    def __init__(self, id):
-        """_summary_"""
+    def __init__(self, id, **params):
+        """Construct the QCPanel object"""
+        super().__init__(**params)
+
         self.id = id
 
         self.submit_button = pn.widgets.Button(
@@ -42,20 +43,35 @@ class QCPanel:
             self._has_data = True
         else:
             return
+        
+        if "data_description" in json_data:
+            self.modalities = [modality['abbreviation'] for modality in json_data["data_description"]["modality"]]
 
         update_schema_version(json_data)
 
-        self.name = json_data["name"]
+        self.asset_name = json_data["name"]
         try:
             self._data = QualityControl.model_validate_json(json.dumps(json_data["quality_control"]))
+
+            self.stages = []
+            # parse stages from QC data
+            for evaluation in self._data.evaluations:
+                if evaluation.stage not in self.stages:
+                    self.stages.append(evaluation.stage)
+
         except Exception as e:
             self._data = None
             print(f"QC object failed to validate: {e}")
+            return
 
         self.evaluations = []
-        for evaluations in self._data.evaluations:
+        self.evaluation_filters = []
+        for evaluation in self._data.evaluations:
+            self.evaluation_filters.append(
+                (evaluation.stage, evaluation.modality.abbreviation)
+            )
             self.evaluations.append(
-                QCEvalPanel(parent=self, qc_evaluation=evaluations)
+                QCEvalPanel(parent=self, qc_evaluation=evaluation)
             )
 
     @property
@@ -73,18 +89,34 @@ class QCPanel:
         self.submit_button.disabled = True
         self.hidden_html.object = "<script>window.location.reload();</script>"
 
+    def _update_modality_filter(self, event):
+        self.modality_filter = event.new
+
+    def _update_stage_filter(self, event):
+        self.stage_filter = event.new
+
+    @param.depends('modality_filter', 'stage_filter', watch=True)
+    def update_objects(self):
+        objects = []
+        for evaluation, filters in zip(self.evaluations, self.evaluation_filters):
+            (stage, modality) = filters
+            if (
+                not (self.modality_filter != "All" and modality != self.modality_filter)
+                and
+                not (self.stage_filter != "All" and stage != self.stage_filter)
+            ):
+                objects.append(evaluation.panel())
+
+        self.tabs.objects = objects
+
     def panel(self):
         """Build a Panel object representing this QC action"""
         if not self._has_data:
             return pn.widgets.StaticText("No QC object available")
 
-        objects = []
-        for evaluation in self.evaluations:
-            objects.append(evaluation.panel())
-
         # build the header
         md = f"""
-# Quality control for {self.name}
+# Quality control for {self.asset_name}
 """
         header = pn.pane.Markdown(md)
 
@@ -112,13 +144,27 @@ class QCPanel:
             quality_control_pane, pn.HSpacer(), self.submit_button
         )
 
-        tabs = pn.Tabs(sizing_mode='stretch_width')
-        tabs.objects = objects
+        # filters for modality and stage
+        self.modality_selector = pn.widgets.Select(
+            name="Modality",
+            options=["All"] + self.modalities,
+        )
+        self.stage_selector = pn.widgets.Select(
+            name="Stage",
+            options=["All"] + self.stages,
+        )
 
-        col = pn.Column(header_row, pn.layout.Divider(), tabs, self.hidden_html)
+        self.modality_selector.param.watch(self._update_modality_filter, "value")
+        self.stage_selector.param.watch(self._update_stage_filter, "value")
 
-        body = col
-        return body
+        header_col = pn.Column(header_row, pn.Row(self.modality_selector, self.stage_selector))
+
+        self.tabs = pn.Tabs(sizing_mode='stretch_width')
+        self.update_objects()
+
+        col = pn.Column(header_col, pn.layout.Divider(), self.tabs, self.hidden_html)
+
+        return col
 
     def dump(self):
         """Return this quality_control.json object back to it's JSON format"""
