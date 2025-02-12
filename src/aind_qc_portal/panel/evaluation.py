ca7aacadefdeda9ec9b8b86f3564dae6ffb98a42
Dan Birman <danbirman@gmail.com>
2025-02-12 11:12:19 -0800

    chore: lint

--- src/aind_qc_portal/panel/evaluation.py
+++ src/aind_qc_portal/panel/evaluation.py
@@ -82,9 +82,11 @@ class QCEvalPanel:
 
         allow_failing_str = "Metrics are allowed to fail." if self._data.allow_failed_metrics else ""
 
+        now = datetime.now(tz=timezone.utc)
+
         md = f"""
 {replace_markdown_with_html(12, self._data.description if self._data.description else "*no description provided*")}
-{replace_markdown_with_html(8, f"Current state: **{qc_status_html(self._data.status(date=datetime.now(tz=timezone.utc)))}**")}
+{replace_markdown_with_html(8, f"Current state: **{qc_status_html(self._data.status(date=now))}**")}
 {replace_markdown_with_html(8, f"Contains **{len(self._data.metrics)}** metrics. {allow_failing_str}")}
 """
 
