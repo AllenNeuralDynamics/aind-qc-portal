d7b5653cf1ddc3fa72b33e3b1825cef3f2c1dbe6
Dan Birman <danbirman@gmail.com>
2025-08-19 22:14:36 -0700

    feat: starting on portal and portal database

---
+++ src/aind_qc_portal/portal.py
@@ -0,0 +1,3 @@
+"""Portal main entrypoint"""
+
+import panel as pn
---
+++ src/aind_qc_portal/portal/database.py
@@ -0,0 +1,75 @@
+"""Database for the Portal app
+
+The database pulls _id information and more complete record information from DocDB
+and uses to populate the view for the user. Aggressive caching is used to prevent
+updaetes to the database from causing excessive load on the DocDB server and to reduce
+page load times for users looking at large numbers of records.
+"""
+
+from datetime import datetime
+from typing import Optional
+from aind_data_access_api.document_db import MetadataDbClient
+
+client = MetadataDbClient(
+    host="https://allenneuraldynamics.org",
+    version="v2",
+)
+
+FIELDS = [
+    "name",
+]
+
+
+class Database:
+    """Database for the Portal app"""
+
+    def build_query(
+        self,
+        name_string: Optional[str] = None,
+        project_name: Optional[str] = None,
+        subject_id: Optional[str] = None,
+        start_date: Optional[datetime] = None,
+        end_date: Optional[datetime] = None,
+    ):
+        """Construct a MongoDB query using the available parameters"""
+
+        query = {}
+
+        if name_string:
+            query["name"] = {"$regex": name_string, "$options": "i"}
+
+        if project_name:
+            query["data_description.project_name"] = project_name
+
+        if subject_id:
+            query["subject.subject_id"] = subject_id
+
+        # [TODO] Add date filtering
+
+        return query
+
+    def get_ids(self, query: dict):
+        """Get a list of record IDs matching the query"""
+
+        try:
+            ids = client.retrieve_docdb_records(
+                filter_query=query,
+                projection={"_id": 1},
+            )
+            return ids
+        except Exception as e:
+            print(f"Error fetching IDs: {e}")
+            return []
+
+    def get_records(self, query: dict):
+        """Get complete records matching the query"""
+
+        try:
+            records = client.retrieve_docdb_records(
+                filter_query=query,
+                projection={f"{field}": 1 for field in FIELDS},
+            )
+            return records
+        except Exception as e:
+            print(f"Error fetching records: {e}")
+            return []
