8c023e05e2a24f2948b0ca93a802307daa6ec09e
Dan Birman <danbirman@gmail.com>
2025-08-19 22:19:06 -0700

    chore: lint

--- src/aind_qc_portal/portal/panel.py
+++ src/aind_qc_portal/portal/panel.py
@@ -5,12 +5,11 @@ from panel.custom import PyComponent
 
 
 class Portal(PyComponent):
-    
-    
+
     def __init__(self):
         """Initialize the Portal app"""
         self.panel = pn.Column("here")
-    
+
     def __panel__(self):
         """Return the Panel representation of the Portal app"""
         return self.panel
