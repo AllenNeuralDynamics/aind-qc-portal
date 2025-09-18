ae1975597a99898289c8cbc5668ddf20362e4ffd
Dan Birman <danbirman@gmail.com>
2025-09-17 21:07:48 -0700

    docs: improving docs

--- upload_testing/test_metadata.py
+++ upload_testing/test_metadata.py
@@ -1,3 +1,8 @@
+""" Upload a test metadata.json file to a running QC portal server
+
+Once uploaded you can access the metadata at <server_url>/view?name=multiplane-ophys_692478_2023-09-29_08-53-45_processed_2025-04-22_20-50-47
+"""
+
 import json
 import requests
 
@@ -8,7 +13,5 @@ with open('metadata.json', 'r') as f:
 # Upload it
 # response = requests.post('https://qc.allenneuraldynamics-test.org/upload_metadata', json=metadata)
 response = requests.post('http://localhost:5007/upload_metadata', json=metadata)
-
-# Name: multiplane-ophys_692478_2023-09-29_08-53-45_processed_2025-04-22_20-50-47
 
 print(f"Status: {response.status_code}")
